b5b52ad46a964e8effe9aa9d5675bf62b5b471a0
helen-lo <kl2739@barnard.edu>
2017-03-21 14:55:16 -0400

    update

--- lib/train.R
+++ lib/train.R
@@ -1,37 +1,50 @@
-#########################################################
-### Train a classification model with training images ###
-#########################################################
+#############################################################################
+### Train baseline and advanced classification model with training images ###
+#############################################################################
 
-### Author: Yuting Ma
-### Project 3
-### ADS Spring 2016
-
-
-train <- function(dat_train, label_train, par=NULL){
-  
-  ### Train a Gradient Boosting Model (GBM) using processed features from training images
-  
+train <- function(dat_train, label_train){
   ### Input: 
-  ###  -  processed features from images 
-  ###  -  class labels for training images
-  ### Output: training model specification
+  ###  -  R object that contains processed training set features 
+  ###  -  R object of training sample labels.
+  ### Output: 
+  ###      RData file that contains trained classifiers 
+  ###      in the forms of R objects
   
   ### load libraries
   library("gbm")
-  
+
+##########============BASELINE MODEL============##########
   ### Train with gradient boosting model
-  if(is.null(par)){
-    depth <- 3
-  } else {
-    depth <- par$depth
-  }
+    ##use 'best' parameters from training images
+    depth <- best_params$depth 
+
+  ##fit the model with specified depth parameter
   fit_gbm <- gbm.fit(x=dat_train, y=label_train,
                      n.trees=2000,
                      distribution="bernoulli",
                      interaction.depth=depth, 
                      bag.fraction = 0.5,
                      verbose=FALSE)
+  #check performance: best number of trees/iterations?  
   best_iter <- gbm.perf(fit_gbm, method="OOB", plot.it = FALSE)
-
-  return(list(fit=fit_gbm, iter=best_iter))
+  ##fit the model with best depth parameter and best n.trees parameter
+  best_fit_gbm <- gbm.fit(x=dat_train, y=label_train,
+                           n.trees=best_iter,
+                           distribution="bernoulli",
+                           interaction.depth=depth, 
+                           bag.fraction = 0.5,
+                           verbose=FALSE)
+  
+##########============ADVANCED MODEL============##########
+  #source("***.R")
+  #best_fit_adv <- train.***(dat_train,label_train)
+  
+##########============OUTPUT DATA============##########
+  
+  # output = list(baseline_fit = gbm.object , advanced_fit =  *** object)
+  output <- list(baseline_fit=best_fit_gbm, advanced_fit=best_fit_adv)
+  save(output,file="output/trained_models.RData")
+  return(output)
+  
+  
 }
