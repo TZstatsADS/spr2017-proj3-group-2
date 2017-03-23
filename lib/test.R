cc9f5cd1eeed721594fdc23ea390725369ee8c2f
helen-lo <kl2739@barnard.edu>
2017-03-23 17:06:01 -0400

    update test.r

--- lib/test.R
+++ lib/test.R
@@ -6,7 +6,7 @@
 ######################################################
 
 
-test <- function(fit_train, dat_test, model="both"){
+test <- function(fit_train, dat_test_base=NULL, dat_test_adv=NULL, model="both"){
   
   ### Fit the classfication model with testing data
   
@@ -22,13 +22,13 @@ test <- function(fit_train, dat_test, model="both"){
   if (model == "both"){
   ##########============BASELINE MODEL============##########
   #fit_train$baseline_fit is a gbm.object class object
-    pred_base <- predict(fit_train$baseline_fit, newdata=dat_test, 
+    pred_base <- predict(fit_train$baseline_fit, newdata=dat_test_base, 
                          n.trees = fit_train$base_best_iter,
                          type="response")
   
   ##########============ADVANCED MODEL============##########
   #fit_train$advanced_fit is a xgb.Booster class object
-    pred_adv <- predict(fit_train$advanced_fit, newdata=dat_test)
+    pred_adv <- predict(fit_train$advanced_fit, newdata=dat_test_adv)
                         #Note: Since we pass a xgb.Booster model object already fitted
                         #      with the best nrounds parameter (i.e. best iteration/n.trees
                         #      for boosting), then we can leave out the
@@ -47,7 +47,7 @@ test <- function(fit_train, dat_test, model="both"){
   else if (model == "base"){
     ##########============BASELINE MODEL============##########
     #fit_train$baseline_fit is a gbm.object class object
-    pred_base <- predict(fit_train$baseline_fit, newdata=dat_test, 
+    pred_base <- predict(fit_train$baseline_fit, newdata=dat_test_base, 
                          n.trees = fit_train$base_best_iter,
                          type="response")
     
@@ -58,7 +58,7 @@ test <- function(fit_train, dat_test, model="both"){
   else if (model == "advanced"){
     ##########============ADVANCED MODEL============##########
     #fit_train$advanced_fit is a xgb.Booster class object
-    pred_adv <- predict(fit_train$advanced_fit, newdata=dat_test)
+    pred_adv <- predict(fit_train$advanced_fit, newdata=dat_test_adv)
     
     output_adv <- as.numeric(pred_adv > 0.5)
     return(output_adv)
